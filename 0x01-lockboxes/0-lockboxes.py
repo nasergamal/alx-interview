a7e554356f6454cd549292dc98e3053f1213cf98
nasergamal <naser.gamal98@gmail.com>
2023-12-05 13:24:36 +0200

    handling boxes count

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,11 +6,17 @@ def canUnlockAll(boxes):
     '''
        boxes list of lists: boxes that contain 0 or more keys
     '''
-    keys = {0}
+    keys = [0]
     i = 0
+    boxes_count = len(boxes) - 1
     while i < len(keys):
-        keys.update(boxes[i])
+        if keys[i] > boxes_count:
+            i += 1
+            continue
+        for key in boxes[keys[i]]:
+            if key not in keys:
+                keys.append(key)
         i += 1
-    if all(key in keys for key in range(len(boxes) - 1)):
+    if all(key in keys for key in range(boxes_count + 1)):
         return True
     return False
