696bda9fc92ba47458dbdb85e8725b16c8758de0
nasergamal <naser.gamal98@gmail.com>
2023-12-19 02:34:53 +0200

    Log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''log parsing'''
+import signal
+from sys import stdin
+i = 0
+size = 0
+possible = ['200', '301', '400', '401', '403', '404', '405', '500']
+res = {}
+
+
+def output(filesize, results):
+    '''printing current stats'''
+    print('File size:', filesize)
+    for k, v in sorted(results.items()):
+        print(f'{k}: {v}')
+
+
+try:
+    for line in stdin:
+        content = line.split()
+        try:
+            if content[7] in possible:
+                res[content[7]] = res.get(content[7], 0) + 1
+                size += int(content[8])
+        except IndexError:
+            pass
+        i += 1
+        if i == 10:
+            output(size, res)
+            i = 0
+except KeyboardInterrupt:
+    output(size, res)
+    quit
+
+if i > 0:
+    output(size, res)
