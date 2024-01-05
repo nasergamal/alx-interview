66536e12e2be2a62cc6bbcf7c1f40b0241f3f9c2
nasergamal <naser.gamal98@gmail.com>
2024-01-05 15:06:07 +0200

    method change

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,24 +2,20 @@
 """
 UTF-8 Validation
 """
-import chardet
-from sys import getsizeof
-byte = {'11110': 5, '1110': 4, '110': 3}
 
 
 def validUTF8(data):
     byte_count = 0
     for i in data:
+        c = 1 << 7
         if byte_count == 0:
-            if ("{0:08b}".format(i)).startswith(tuple(byte)):
-                for k, v in byte.items():
-                    if "{0:08b}".format(i)[:v] in k:
-                        byte_count = v - 2
-                        break
-            elif i >> 7 != 0:
-                return False
+            while c & i:
+                c >>= 1
+                byte_count += 1
+            if byte_count == 0:
+                continue
         else:
-            if "{0:08b}".format(i)[:2] != '10':
+            if not (i & 1 << 7):
                 return False
-            byte_count -= 1
+        byte_count -= 1
     return byte_count == 0
